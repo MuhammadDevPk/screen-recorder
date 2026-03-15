72802430ca0fa1be249298fc58365715deec6cf9
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-03-15 19:03:02 +0500

    Create actions.ts

---
+++ app/actions.ts
@@ -0,0 +1,8 @@
+// 'use server';
+
+// import Mux from '@mux/mux-node';
+
+// const mux = new Mux({
+//     tokenId: process.env.MUX_TOKEN_ID,
+//     tokenSecret: process.env.MUX_TOKEN_SECRET,
+// });
