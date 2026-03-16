f130f26987dd78ec7ee5af01dd0b1e4693b28546
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-03-16 14:20:47 +0500

    added configurations for upload download video

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,27 @@
-// 'use server';
+'use server';
 
-// import Mux from '@mux/mux-node';
+import Mux from '@mux/mux-node';
 
-// const mux = new Mux({
-//     tokenId: process.env.MUX_TOKEN_ID,
-//     tokenSecret: process.env.MUX_TOKEN_SECRET,
-// });
+const mux = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
+
+
+export async function createUploadUrl() {
+    const upload = await mux.video.uploads.create({
+        new_asset_settings: {
+            playback_policy: ['public'],
+            video_quality: 'plus',
+            mp4_support: 'standard',
+            input: [
+                {
+                    generated_subtitles: [
+                        {language_code: 'en', name: 'English (Auto)'}
+                    ]
+                }
+            ]
+        },
+        cors_origin: '*',
+    });
+}
