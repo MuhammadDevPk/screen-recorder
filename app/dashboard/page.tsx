988533e29e68b99e3942a7554a3970b268983471
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-01 12:40:46 +0500

    created component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,3 +2,10 @@ import Link from 'next/link';
 import { listVideos } from '@/app/actions';
 import { ArrowLeft } from 'lucide-react';
 import VideoThumbnail from '@/components/VideoThumbnail';
+
+export const dynamic = 'force-dynamic';
+
+export default async function DashboardPage() {
+    const videos = await listVideos();
+
+}
