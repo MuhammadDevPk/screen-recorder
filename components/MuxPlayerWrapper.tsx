7fee58a8d297653a58423a259b279e10e974f200
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-02 11:44:09 +0500

    defined toekn for security in mux wrapper

--- components/MuxPlayerWrapper.tsx
+++ components/MuxPlayerWrapper.tsx
@@ -5,12 +5,18 @@ import MuxPlayer from '@mux/mux-player-react';
 interface MuxPlayerWrapperProps {
     playbackId: string;
     title?: string;
+    token?: string;
 }
 
-export default function MuxPlayerWrapper({ playbackId, title }: MuxPlayerWrapperProps) {
+export default function MuxPlayerWrapper({ playbackId, title, token }: MuxPlayerWrapperProps) {
     return (
         <MuxPlayer
         playbackId={playbackId} 
+        tokens={token ? {
+            playback: token,
+            thumbnail: token,
+            storyboard: token,
+        } : undefined}
         metadata={{
             video_title: title || 'Screen Recording'
         }}
