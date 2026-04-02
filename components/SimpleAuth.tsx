657d48742ef72e9b6220d8094958301a51f91256
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-02 11:31:28 +0500

    call login, logout api

--- components/SimpleAuth.tsx
+++ components/SimpleAuth.tsx
@@ -3,3 +3,22 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+export default function SimpleAuth({ currentUser }: { currentUser: string | null }) {
+    const [userName, setUserName] = useState('');
+    const router = useRouter();
+
+    const handleLogin = () => {
+        if (!userName.trim()) return;
+        await fetch('/api/fake-login', {
+            method: 'POST',
+            body: JSON.stringify({userName}),
+        });
+        router.refresh();
+    };
+
+    const handleLogout = async () => {
+        await fetch('/api/fake-logout', {method: 'DELETE'});
+        router.refresh();
+    }
+
+}
